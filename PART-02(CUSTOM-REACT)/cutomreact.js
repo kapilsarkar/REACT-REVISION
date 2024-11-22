361bb829c4293fda5d46b8b2825d8d32b0f4a02d
kapilsarkar <k.sarkar81@gmail.com>
2024-11-22 17:09:04 +0530

    somepartsadded

--- PART-02(CUSTOM-REACT)/cutomreact.js
+++ PART-02(CUSTOM-REACT)/cutomreact.js
@@ -1,1 +1,17 @@
+function customRender(reactElement,container){
+  const domElement = document.createElement(reactElement.type)
+  domElement.in
+}
+
+const reactElement = {
+    type:'a',
+    props:{
+        href:'https://google.com',
+        target:'_blank',
+    },
+    children: 'Click me to visit google',
+}
+
 const mainContainer = document.querySelector("#root");
+
+customRender(reactElement,mainContainer)
