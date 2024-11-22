28538a5c8c063c120c15b36391afab9348b87f11
kapilsarkar <k.sarkar81@gmail.com>
2024-11-22 08:47:26 +0530

    somepartsadded

--- PART-01/src/App.jsx
+++ PART-01/src/App.jsx
@@ -1,7 +1,10 @@
+import Kapil from "./Kapil";
+
 function App() {
   return (
     <>
       <h2>Hello Kapil Sarkar learn React with Vite</h2>
+      <Kapil/>
     </>
   );
 }
---
+++ PART-01/src/Kapil.jsx
@@ -0,0 +1,5 @@
+const Kapil = () => {
+  return <h2>Hello Kapil Sarkar Welcome Your Time is Now</h2>;
+};
+
+export default Kapil;
