c42bf8dbb195ce7e737f1f61a0ea6c4856517c41
kapilsarkar <k.sarkar81@gmail.com>
2024-11-24 08:05:40 +0530

    somepartsadded

--- PART-03(COUNTER-APP)/src/App.jsx
+++ PART-03(COUNTER-APP)/src/App.jsx
@@ -1,16 +1,25 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  
+  const [counter, setCounter] = useState(0);
 
+  const addValue = () => {
+    if (counter < 20) setCounter(counter + 1);
+  };
+
+  const removeValue = () => {
+    if (counter > 0) setCounter(counter - 1);
+  };
   return (
     <>
       <h1>Counter-App</h1>
-      <h2>Counter-Value</h2>
-      <button>Add Value</button>
-      <button>Remove Value</button>
+      <h2>Counter-Value:{counter}</h2>
+      <button onClick={addValue}>Add Value:{counter}</button>
+      <button onClick={removeValue}>Remove Value:{counter}</button>
+      <p>Footer:{counter}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
