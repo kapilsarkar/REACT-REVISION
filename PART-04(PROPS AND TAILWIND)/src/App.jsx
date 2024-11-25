b9893a8e2e37f6208635404679f9580b1236ba04
kapilsarkar <k.sarkar81@gmail.com>
2024-11-25 07:29:21 +0530

    somepartsadded

---
+++ PART-04(PROPS AND TAILWIND)/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-900 text-white p-3 rounded-xl mb-2">
+        Hi Kapil Welcome tO React+Vite with Tailwind CSS
+      </h1>
+     <Card/>
+    </>
+  );
+}
+
+export default App;
