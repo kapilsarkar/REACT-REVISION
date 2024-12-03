9b8e8d810f9aaed8acd21ecaa04a19970e076e3d
kapilsarkar <k.sarkar81@gmail.com>
2024-12-03 08:40:07 +0530

    somepartsadded

--- PART-08(REACT-ROUTER)/src/main.jsx
+++ PART-08(REACT-ROUTER)/src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./components/Home";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element : <Layout/>,
+    children :[
+      {
+        path:"",
+        element: <Home/>,
+      },
+      {
+        path:"about",
+        element: < About/>,
+      },
+      {
+        path:"contact",
+        element:<Contact/>
+      }
+    ]
+  }
+])
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}/>
+  </StrictMode>
+);
