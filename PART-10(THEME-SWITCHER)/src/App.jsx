cf7a8680b46ec85a9544878523a62b9c316c49e6
kapilsarkar <k.sarkar81@gmail.com>
2024-12-12 17:34:20 +0530

    somepartsadded

--- PART-10(THEME-SWITCHER)/src/App.jsx
+++ PART-10(THEME-SWITCHER)/src/App.jsx
@@ -1,9 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <>
-  <h2 className="p-2 bg-pink-500 text-3xl">THEME SWITCHER</h2>
-  </>;
+  return (
+    <>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* themeBtn */}
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            {/* Card */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
