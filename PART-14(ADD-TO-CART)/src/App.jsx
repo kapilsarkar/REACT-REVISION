84adb4aa1f2b7233581feb9c3ea8d2e74a3a5d7e
kapilsarkar <k.sarkar81@gmail.com>
2025-01-05 08:13:30 +0530

    somepartsadded

--- PART-14(ADD-TO-CART)/src/App.jsx
+++ PART-14(ADD-TO-CART)/src/App.jsx
@@ -1,14 +1,10 @@
-import Cart from "./components/Cart";
-import Item from "./components/Item";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <h1>Add To Cart</h1>
-      <Item name="MacBook Pro" price={10000}/>
-      <Item name="Pendrive" price={5000}/>
-      <Item name="Mobile" price={36000}/>
-      <Cart/>
+      <Header />
     </>
   );
 }
